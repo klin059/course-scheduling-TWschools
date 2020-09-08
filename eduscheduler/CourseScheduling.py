82cd91fcc16eb7838b2a0d42e908b89257246336
Jeff L <17500820+klin059@users.noreply.github.com>
2020-09-08 23:57:33 +0800

    fix same_period requirement combined with other requirements from related courses

--- eduscheduler/CourseScheduling.py
+++ eduscheduler/CourseScheduling.py
@@ -15,71 +15,80 @@ def large_neighbourhood_search_with_scoring(status, max_iteration = 300):
     global_solution = status.make_copy()
     global_score = scoring.get_status_penalty(global_solution)
     
-#    max_n_assigned = len(global_solution.list_of_assigned_Courses)
     local_solution = status.make_copy()
     n_samples_remove = int(len(local_solution.list_of_assigned_Courses)/3)
     n_samples_addon = int(len(local_solution.list_of_unassigned_Courses)/3)
     
     iteration = 0
-#    local_solution.assert_status()
     while not iteration > max_iteration:
-        
+        # decide the fraction of courses to be rescheduled
         fraction = 3
         if len(local_solution.list_of_unassigned_Courses) < 10:
             fraction = 1
         n_samples_remove = int(len(local_solution.list_of_assigned_Courses)/3)
         n_samples_addon = int(len(local_solution.list_of_unassigned_Courses)/fraction)
-#        local_solution.assert_status()
-        Courses_to_remove = random.sample(local_solution.list_of_assigned_Courses,n_samples_remove)
-        Courses_to_add = random.sample(local_solution.list_of_unassigned_Courses,n_samples_addon)
+        
+        Courses_to_remove = random.sample(local_solution.list_of_assigned_Courses, n_samples_remove)
+        Courses_to_add = random.sample(local_solution.list_of_unassigned_Courses, n_samples_addon)
         removed_Courses = []
+        # unassign courses
         for course in Courses_to_remove:
-            
             if "same_period" in course.requirements:
                 for C in course.requirements["same_period"]:
                     if C.assigned is True:
                         C.unassign_course(local_solution)
-                    assert(C.assigned == False)
                     removed_Courses.append(C)
             else:
                 course.unassign_course(local_solution)
                 removed_Courses.append(course)
+        
+        # assign courses
         Courses_to_add = Courses_to_add + removed_Courses
-
-        local_solution.assert_status()
         for course in Courses_to_add:
-
-            
-            local_solution.assert_status()
             feasible_periods = bm.Course.populate_set_of_periods()
             feasible_periods = course.get_feasible_periods_by_requirement(local_solution, feasible_periods) 
-            if feasible_periods == set():
-#                print("cannot find a feasible period for {}".format(course.name))
-                continue
-            period = random.sample(feasible_periods, 1)[0]
-            # for same_period requirement, need to assign all the relevant courses in the same iteration loop
+            
+            # if same_period requirement, get feasible_periods for courses with the same requriement
+            course_assigned = False
             if "same_period" in course.requirements:
-                # assign period to all courses that have this requirement
-    #            print("same_period:{}".format([C.name for C in course.requirements["same_period"]]))
                 for C in course.requirements["same_period"]:
+                    # any one of the related course is assigned, skip the course
                     if C.assigned is True:
-                        continue
-                    else:
-                        C.assign_course_period(local_solution, period)                    
+                        course_assigned = True
+                        break
+                    feasible_periods = C.get_feasible_periods_by_requirement(local_solution, feasible_periods)
+            if feasible_periods == set() or course_assigned:
+                continue    
+            period = random.sample(feasible_periods, 1)[0]
+            if "same_period" in course.requirements:
+                for C in course.requirements["same_period"]:
+                    C.assign_course_period(local_solution, period)  
             else:
                 course.assign_course_period(local_solution, period)
+#            # for same_period requirement, need to assign all the relevant courses in the same iteration loop
+#            if "same_period" in course.requirements:
+##                # assign period to all courses that have this requirement
+##                for C in course.requirements["same_period"]:
+##                    if C.assigned is True:
+##                        continue
+##                    else:
+##                        C.assign_course_period(local_solution, period)                    
+#                                # assign period to all courses that have this requirement
+#                for C in course.requirements["same_period"]:
+#                    if C.assigned is True:
+#                        continue
+#                    feasible_periods = C.get_feasible_periods_by_requirement(local_solution, feasible_periods)
+#                for C in course.requirements["same_period"]:
+#                    C.assign_course_period(local_solution, period)  
+#            else:
+#                course.assign_course_period(local_solution, period)
                 
         # update global solution
         local_score = scoring.get_status_penalty(local_solution)
         if  local_score < global_score:
-           
             global_solution = local_solution.make_copy()
-#            global_solution.assert_status()
             global_score = local_score
-        
-#        if global_solution.list_of_unassigned_Courses == []:
-#            print('all {} courses assigned using {} iteration'.format(n_Courses_to_assign, iteration))
-#            return global_solution     
+           
         iteration += 1
     n_Courses_to_assign = len(global_solution.list_of_Courses) - len(global_solution.list_of_stalled_Courses) - len(global_solution.list_of_fixed_Courses)
     print('Assigned {} out of {} courses'.format(len(global_solution.list_of_assigned_Courses), str(n_Courses_to_assign)))
